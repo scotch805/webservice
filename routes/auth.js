9dd1f576684e0542c9632a5f0b93e615c7137401
scotch805 <won42100@naver.com>
2023-08-18 15:20:39 +0900

    modified by owner

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ module.exports = {
     CheckAuth: async function(req, res, next) {
         console.log(req);
         console.log(req);
+        console.log(req);
+        console.log(req);
         if(req.isAuthenticated()) {
             next();
         }
